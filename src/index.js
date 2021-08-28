c62650bedcefdacda6cb3495f6a4ef39e3788727
Vladislav Shabusov <vladshabusov66@gmail.com>
2021-08-29 00:16:44 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = {};  //Объект с парами скобок
+
+  let sameBracketsArr = [];
+  function getSameBrackets (arr) {
+    for (let key in brackets) {
+      if (key === brackets[key]) {
+        arr.push(key);
+      }
+    }
+    return arr;
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let key = bracketsConfig[i][0];
+    brackets[key] = bracketsConfig[i][1];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (Object.keys(brackets).includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+  
+      let topElement = stack[stack.length-1];
+      function openBracket(value) {
+        for (let key in brackets) {   //поиск открывающей скобки к текущей закрывающей
+          if (brackets[key] === value) {
+            return key;
+          }
+        }
+        return null;
+      }
+      
+      if (topElement === openBracket(str[i])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    if (stack[stack.length-2] === stack[stack.length-1] && getSameBrackets(sameBracketsArr).includes(stack[stack.length-1])) {
+      stack.pop();
+      stack.pop();
+  }
+  }
+  return stack.length === 0;
 }
